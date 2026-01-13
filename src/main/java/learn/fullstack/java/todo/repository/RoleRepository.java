9e37739e4879a1aa6466662433ab0b6dfbf1794d
Praveen Raaj <praveenraajm@outlook.com>
2026-01-13 22:09:22 +0530

    database auth with custom user details service

---
+++ src/main/java/learn/fullstack/java/todo/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package learn.fullstack.java.todo.repository;
+
+import learn.fullstack.java.todo.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
